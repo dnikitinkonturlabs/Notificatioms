12c9df7b96fd0d21508b3315f1b845e078e103d9
dmitry_nikitin <dnikitin@konturlabs.com>
2017-11-23 15:54:57 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
 
 firebase.initializeApp({
-    'messagingSenderId': '145852524729'
+    'messagingSenderId': '893318301019'
 });
 
 firebase.messaging();
